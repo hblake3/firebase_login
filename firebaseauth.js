572707ff5447c9b473709edcb521af06d7e77418
hblake3 <155304179+hblake3@users.noreply.github.com>
2024-10-22 15:09:08 -0400

    .env, .gitignore

--- firebaseauth.js
+++ firebaseauth.js
@@ -5,13 +5,13 @@ import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} fro
 import {getFirestore, setDoc, doc} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDp6O9xskslUCSk8bqlHQLJz8HGFidUvwo",
-    authDomain: "login-349f2.firebaseapp.com",
-    projectId: "login-349f2",
-    storageBucket: "login-349f2.appspot.com",
-    messagingSenderId: "1062053335811",
-    appId: "1:1062053335811:web:5a8c40851e8d43d6c98f36"
-  };
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
